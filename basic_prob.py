17b0b049c84f317f90fdfc957daf27a9b9e2bb81
Nikila-B <nikila22babu@gmail.com>
2021-06-22 23:50:37 +0530

    Added random variables along with probability mass function
    Co-authored-by: SaiKrishna I <ArachnidAgrajag@users.noreply.github.com>

--- basic_prob.py
+++ basic_prob.py
@@ -1,3 +1,4 @@
+from typing import Any
 def sqrt(num : float )->float:
     '''Returns the square root of the given number'''
     xn=1
@@ -11,16 +12,16 @@ def sqrt(num : float )->float:
             xn = xn1 
     return xn
 
-def greater(col : list[float|int], val:Any)->list[bool]:
+def greater(col : list[float], val:Any)->list[bool]:
     '''Returns a boolean list that has true if the corresponding list element is greater than the given number'''
     g = lambda x : True if x >val else False
     return [x>val for x in col]
 
-def equal(col : list[float|int], val:Any)->list[bool]:
+def equal(col : list[float], val:Any)->list[bool]:
     '''Returns a boolean list that has true if the corresponding list element is equal to the given number'''
     return [x==val for x in col]
 
-def less(col : list[float|int], val:Any)->list[bool]:
+def less(col : list[float], val:Any)->list[bool]:
     '''Returns a boolean list that has true if the corresponding list element is lesser than the given number'''
     return [x<val for x in col]
 
@@ -70,34 +71,34 @@ def median(col : list[bool])->float:
     ''' returns median of the column'''
     return percentile(col,50)
 
-def minimum(col : list[float|int])->float|int:
+def minimum(col : list[float])->float:
     ''' returns minimum list element'''
     return percentile(col,1)
 
-def maximum(col : list[float|int])->float|int:
+def maximum(col : list[float])->float:
     ''' returns maximum list element'''
     return percentile(col,100)
 
-def first_quart(col : list[float|int])->float|int:
+def first_quart(col : list[float])->float:
     ''' returns 25th percentile of the column'''
     return percentile(col,25)
 
-def third_quart(col : list[float|int])->float|int:
+def third_quart(col : list[float])->float:
     ''' returns 75th percentile of the column'''
     return percentile(col,75)
 
-def iqr(col : list[float|int])->float|int:
+def iqr(col : list[float])->float:
     ''' returns difference between the 2 quartiles'''
     return percentile(col,75) - percentile(col,25)
 
-def percentile(col : list[float|int],per : int)->list:
+def percentile(col : list[float],per : int)->list:
     ''' returns nth percentile of the column'''
     if per>=1 and per<=100:
         return sorted(col)[round((per/100)*(len(col)))-1]
     else:
         raise ValueError("Percentile value should be between 1 and 100")
 
-def trimmed_mean(col : list[float|int],per : int)->float:
+def trimmed_mean(col : list[float],per : int)->float:
     ''' returns the trimmed_mean'''
     if per>=1 and per<=100:
         k = round((per/100)*len(col))
@@ -105,26 +106,31 @@ def trimmed_mean(col : list[float|int],per : int)->float:
     else:
         raise ValueError("Percentage value should be between 1 and 100")
 #sum((x-x_mean)^2)
-def var(col : list[float|int])->float:
+def var(col : list[float])->float:
     ''' returns variance of the column'''
     return sum(square(sub(col,mean())))/(len(col)-1)
 
-def sd(col : list[float|int])->float:
+def sd(col : list[float])->float:
     ''' returns standard deviation of the column'''
     return sqrt(var(col))
 
 def isBoolean(col : list)->bool:
     ''' checks if the entire column is made up of boolean values'''
-    return all(isinstance(x, bool) for x in col)
+    if type(col)==list:
+        return all(isinstance(x, bool) for x in col)
+    else:
+        return False
 
 def prob(col : list[bool])->float:
     ''' returns probability of 1's in the column'''
     if isBoolean(col):
         return mean(col)
+    elif type(col)==dict:
+        return sum(col.values())
     else:
         raise TypeError("Expected a list of Boolean values")
 
-def add(col:list[float],val:float|list)->list[float]:
+def add(col:list[float],val:list)->list[float]:
     ''' returns a list incremented by val if the 2nd arg is a float
 
         returns the elementwise sum if the 2nd argument is a list
@@ -137,7 +143,7 @@ def add(col:list[float],val:float|list)->list[float]:
     else:
         return [x+val for x in col]
 
-def sub(col:list[float],val:float|list)->list[float]:
+def sub(col:list[float],val:list)->list[float]:
     ''' returns a list decremented by val if the 2nd arg is a float
 
         returns the elementwise difference if the 2nd argument is a list
@@ -150,7 +156,7 @@ def sub(col:list[float],val:float|list)->list[float]:
     else:
         return [x-val for x in col]
 
-def mul(col:list[float],val:float|list)->list[float]:
+def mul(col:list[float],val:list)->list[float]:
     ''' returns a list scaled by val if the 2nd arg is a float
 
         returns the elementwise product if the 2nd argument is a list
@@ -185,3 +191,113 @@ def conditional(cond1 :list[bool],cond2:list[bool])-> float:
             raise ValueError("Lists should be of same length")
     else:
         raise TypeError("Expected a list of Boolean values")
+
+def independent(col1:list[bool],col2:list[bool])-> bool:
+    ''' returns true id col1 and col2 are independent'''
+    if isBoolean(col1) and isBoolean(col1):
+        if len(col1) == len(col1):
+            return conditional(col1,col2)==conditional(col2,col1)
+        else:
+            raise ValueError("Lists should be of same length")
+    else:
+        raise TypeError("Expected a list of Boolean values")
+
+def normalize(prob1):
+    s = sum(prob1)
+    return [i/s for i in prob1]
+        
+def total_prob(prob1,col):
+    if type(col) == list:
+        if all(x<=1 for x in prob1.values()):
+            s = 0
+            for key,value in prob1.items():
+                s += value * prob(equal(col,key))
+            return s
+        else:
+            raise ValueError("Probability values should be less than 1")
+    else:
+        if all(x<=1 for x in prob1.values()) and all(x<=1 for x in col.values()):
+            if sum(col.values())==1:
+                s = 0
+                for key,value in prob1.items():
+                    s += value * col[key]
+                return s
+            else:
+                raise ValueError("Sum of probability values of the 2nd argument should be equal to 1")
+        else:
+            raise ValueError("Probability values should be less than 1")
+
+class Discrete_RV:    
+    
+    def __init__(self):
+        self.values = {}  
+    
+    def __init__(self,l1):
+        unique_items = lambda list_with_duplicates: list(dict.fromkeys(list_with_duplicates))
+        if type(l1)==list:
+            if all(isinstance(x,int) or isinstance(x,float) for x in l1):
+                self.values = {x:l1.count(x)/len(l1) for x in set(l1)} 
+            else:
+                raise TypeError("Expected a list of numbers")      
+        elif type(l1)==dict:
+            if all(isinstance(x,int) or isinstance(x,float) for x in l1.keys()):
+                s = sum(l1.values())
+                if s==1:
+                    self.values = l1.copy()
+                elif s>0:
+                    self.values = {x:y for x,y in zip(l1.keys(),normalize(l1.values()))}
+                else:
+                    raise ValueError("Expected positive values")
+            else:
+                raise TypeError("Expected keys to be numbers")  
+        else:
+            raise TypeError("Expected list or dict")
+    
+    def __eq__(self, rhs):
+        if type(rhs)==int or type(rhs)==float:
+            if rhs in self.values.keys():
+                return {rhs:self.values[rhs]}
+            else:
+                return {}
+        elif type(rhs)==type(self):
+            return self.values == rhs.values
+        else:
+            raise ValueError("Expected a dict or int or float")
+    
+    def __lt__(self, rhs):
+        if type(rhs)==int or type(rhs)==float:
+            return {x:y for x,y in self.values.items() if x<rhs}
+        elif type(rhs)==type(self):
+            return self.values < rhs.values
+        else:
+            raise ValueError("Expected a dict or int or float")
+            
+    def __gt__(self, rhs):
+        if type(rhs)==int or type(rhs)==float:
+            return {x:y for x,y in self.values.items() if x>rhs}
+        elif type(rhs)==type(self):
+            return self.values > rhs.values
+        else:
+            raise ValueError("Expected a dict or int or float")
+
+    def __ge__(self, rhs):
+        if type(rhs)==int or type(rhs)==float:
+            return {x:y for x,y in self.values.items() if x>=rhs}
+        elif type(rhs)==type(self):
+            return self.values >= rhs.values
+        else:
+            raise ValueError("Expected a dict or int or float") 
+
+    def __le__(self, rhs):
+        if type(rhs)==int or type(rhs)==float:
+            return {x:y for x,y in self.values.items() if x<=rhs}
+        elif type(rhs)==type(self):
+            return self.values <= rhs.values
+        else:
+            raise ValueError("Expected a dict or int or float") 
+    
+    def pmf(self,key):
+        return prob(self==key)
+    
+    def cdf(self,key):
+        return prob(self<=key)
--- test.py
+++ test.py
@@ -7,10 +7,15 @@ val2 = 75
 print(bp.prob(bp.aand(bp.greater(col,100),bp.less(col,120))))
 a=[0,0,2,6,2,1,5,7,1,1,0,0]
 
-person = ['m','f','m','m','f','m','f','f','f','f']
-shirt = ['r','r','g','r','g','g','r','r','g','r']
-print(bp.conditional(bp.equal(person,'m'),bp.equal(shirt,'r')))
-import math
-bp.sqrt()
-bp.greater()
-bp.mul()
+#person = ['m','m','m','m']
+shirt =  ['r','r','r','g']
+sh = {'r':0.4,'g':0.4}
+p_m = {'r':0.3,'g':1}
+#print(bp.conditional(bp.equal(person,'m'),bp.equal(shirt,'r')) + bp.conditional(bp.equal(person,'m'),bp.equal(shirt,'g')))
+bp.sqrt(25)
+
+l1 = [1,3,4,1,1,2]
+l2 = {1:0.5,2:0.2,3:0.3}
+l3 = {1:0.6,2:0.2}
+a = bp.Discrete_RV(l2)
+print(a.cdf(4))
